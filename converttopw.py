af2d4ac63f9476e8da55b4912881346b8be48995
guoxy <xy.guo@hotmail.com>
2021-09-05 16:39:52 +0200

    Update converttopw.py

--- converttopw.py
+++ converttopw.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-from hashlib import pbkdf2h
+from hashlib import pbkdf2_hmac
+
+lower_case_letters = list("abcdefghijklmnopqrstuvwxyz")
+upper_case_letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+numbers = list("0123456789")
+special_characters = list("#!"§$%&/()=?`':;_–…∞*#")
+password_characters = lower_case_letters + upper_case_letters + numbers + special_characters
+salt = "pepper"
+def convert_bytes_to_password (hashed_bytes, length):
+                          number = int.from_byts (hashed_bytes, byteorder = "big")
+                          password = ""
+                          while number >0 and len(password) < length :
+                          password = password + password_charcters [number % len(password_characters)]
+                          number = number // len(password_characters)
+                          return password
+                          
